bfe20c6d10121aac942ff2d9e96a092ba62d1d65
Svhp7592 <captainamerica7592@gmail.com>
2024-12-17 13:07:29 +0530

    Add files via upload

---
+++ Python 5.py
@@ -0,0 +1,11 @@
+Score = int(input("Enter a Score:"))
+Score = 75
+if Score >= 90:
+    print("A grade")
+elif Score >= 80:
+        print("B grade")
+elif Score >= 75:
+            print("C grade")
+else:
+            print("You are fail")
+        
