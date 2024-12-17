48c8cc2d6d290c9eeb901c556c0f69c102099d89
Svhp7592 <captainamerica7592@gmail.com>
2024-12-17 13:06:00 +0530

    Add files via upload

---
+++ Program3.py
@@ -0,0 +1,15 @@
+integer_var = 0
+double_var = 0.0
+string_var = ""
+
+integer_var = int(input("Enter an integer: "))
+double_var = float(input("Enter a double: "))
+string_var = input("Enter a string: ")
+
+initial_integer = 4
+initial_double = 4.0
+initial_string = "HackerRank "
+
+print(initial_integer + integer_var)
+print(initial_double + double_var)
+print(initial_string + string_var)
