b3858a9c6edd063373daa750dee1eeb859570053
Svhp7592 <captainamerica7592@gmail.com>
2024-12-25 20:03:31 +0530

    Add files via upload

---
+++ Program 11.py
@@ -0,0 +1,4 @@
+for num in range(5):
+    if num == 2:
+        continue
+    print(num)
---
+++ Program 12.py
@@ -0,0 +1,7 @@
+for num in range(10):
+    print(num)
+else:
+    print("Loop Completed")
+    
+        
+    
